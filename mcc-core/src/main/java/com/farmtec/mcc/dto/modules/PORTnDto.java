35ab66dce41f80c28aac61302fae6bb9a554e3cc
dfpaulino <dfpaulino@gmail.com>
2019-02-13 21:25:21 +0000

    add format to hex value

--- mcc-core/src/main/java/com/farmtec/mcc/dto/modules/PORTnDto.java
+++ mcc-core/src/main/java/com/farmtec/mcc/dto/modules/PORTnDto.java
@@ -4,6 +4,7 @@ package com.farmtec.mcc.dto.modules;
 
 import com.farmtec.mcc.dto.BaseDto;
 import com.farmtec.mcc.models.BaseModel;
+import com.fasterxml.jackson.annotation.JsonGetter;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,14 +14,27 @@ import lombok.Setter;
 @Getter @Setter @NoArgsConstructor
 public class PORTnDto extends BaseDto {
 
-    int id;
 
-    String portName;
-    byte value;
+    public int id;
+
+    public String portName;
+
+    private byte value;
     /*data direction
     0 - input
     1- output
      */
-    byte ddb;
+    private byte ddb;
 
+    @JsonGetter("value")
+    public String value()
+    {
+        return "0X"+Integer.toHexString(value & 0xFF);
+    }
+
+    @JsonGetter("ddb")
+    public String ddb()
+    {
+        return "0X"+Integer.toHexString(ddb & 0xFF);
+    }
 }
