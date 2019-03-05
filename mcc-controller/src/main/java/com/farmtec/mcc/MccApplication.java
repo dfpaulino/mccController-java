35a70eb24ede8a9dc8c8405259d1fba8e76a6101
dfpaulino <dfpaulino@gmail.com>
2019-03-05 20:01:38 +0000

    enable Packet handler

--- mcc-controller/src/main/java/com/farmtec/mcc/MccApplication.java
+++ mcc-controller/src/main/java/com/farmtec/mcc/MccApplication.java
@@ -1,5 +1,6 @@
 package com.farmtec.mcc;
 
+import com.farmtec.io.config.EnableIO;
 import com.farmtec.mcc.repositories.config.RepoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication()
 @Import(RepoConfig.class)
+@EnableIO
 public class MccApplication {
 
 	public static void main(String[] args) {
